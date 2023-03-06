f16472b680dbf7bd91a418d3037f38c1b924babd
Kağan Çubukçu <kagancancubukcu@gmail.com>
2023-03-06 14:32:45 +0300

    yedek api eklendi

--- src/api/news-api.services.ts
+++ src/api/news-api.services.ts
@@ -10,7 +10,9 @@ export class NewsService {
 
   getNews(country: string, category: string, page: number): Observable<any> {
     const apiKey = 'e29abe99c41f4dfb92863f94efcacc8b';
-    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${page}`;
+    //yedek api
+    const apiKeyBackup = '1c75f5eb5da4470ba114e6325a69a142';
+    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKeyBackup}&page=${page}`;
 
     return this.http.get(url);
   }
